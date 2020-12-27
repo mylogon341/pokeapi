a1f8aab1a43e9853f67c010a0fd3ba4017c6c047
luke sadler <>
2020-12-27 00:12:59 +0000

    initial

---
+++ dist/requests.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=requests.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const port = 8080; // default port to listen
+
+// start the express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
