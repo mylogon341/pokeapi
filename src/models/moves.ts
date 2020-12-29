861f41ec67987af4d780a4989fee6e0452ce1582
luke sadler <>
2020-12-29 15:08:06 +0000

    added move name to nested data

--- src/models/moves.ts
+++ src/models/moves.ts
@@ -1,18 +1,20 @@
 import { capitalizeFirstLetter, versionNumberFromUrl } from "../Helpers"
 
 class GameVar {
+    move_name: string
     version_name: string
     version_url: string
     version_number: number
     level_learned_at: number
     learned_via: string
 
-    constructor(data: { string: any }) {
+    constructor(moveName, data) {
         this.version_name = capitalizeFirstLetter(data["version_group"].name)
         this.version_url = data["version_group"].url
         this.version_number = versionNumberFromUrl(this.version_url)
         this.level_learned_at = Number(data["level_learned_at"])
         this.learned_via = data["move_learn_method"].name
+        this.move_name = moveName
     }
 }
 
@@ -23,6 +25,6 @@ export class Move {
     constructor(data) {
         this.name = capitalizeFirstLetter(data.move.name)
         const groupDetails: { string: any }[] = data["version_group_details"]
-        this.game_variants = groupDetails.map(g => new GameVar(g))
+        this.game_variants = groupDetails.map(g => new GameVar(this.name, g))
     }
 }
