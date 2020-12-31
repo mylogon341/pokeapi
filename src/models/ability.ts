0c177403cb6e8d6d311786fc1cb91b42ae6b514c
luke sadler <>
2020-12-31 11:56:09 +0000

    add ability id

--- src/models/ability.ts
+++ src/models/ability.ts
@@ -4,10 +4,12 @@ export class Ability {
     description: string
     generation: number
     name: string
+    id: number
     is_hidden: boolean
 
     constructor(data, isHidden: boolean) {
         this.is_hidden = isHidden
+        this.id = data.id
         this.name = capitalizeFirstLetter(data.name)
         this.generation = versionNumberFromUrl(data.generation.url)
         console.log(this.name)
