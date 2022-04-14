5d7ba7a05ed40aab0345c01e2b7de003a2bbc8c5
luke sadler <mylogon@gmail.com>
2022-04-14 18:28:49 +0100

    added url guard

--- src/image_handler.ts
+++ src/image_handler.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-case-declarations */
 import { resolve } from "path"
+import { existsSync } from "fs"
 import { pokemon } from './requests'
 
 export enum ImageSource {
@@ -9,12 +10,14 @@ export enum ImageSource {
 }
 
 export async function getImage(type: ImageSource, index: string): Promise<string> {
-    
+    const nothing = '../node_modules/pokemon-sprites/sprites/items/poke-ball.png'
+    let resolvePath = ""
+
     switch (type) {
         case ImageSource.poke_image:
             
             const poke = await pokemon(index)
-            const path = poke
+            const sprite = poke
             .base
             .sprite
             .groups
@@ -22,14 +25,21 @@ export async function getImage(type: ImageSource, index: string): Promise<string
             .sprite_groups
             .filter(m=>m.group_name == "Official Artwork")[0]
             .sprites[0]
-            .url
-            .imageUrlToPath()
-
-            return resolve("../node_modules/pokemon-sprites" + path)
+            
+            if (sprite != undefined) {
+                const path = sprite.url.imageUrlToPath()
+                resolvePath = "../node_modules/pokemon-sprites" + path
+            }
+            
         case ImageSource.poke_sprite:
-            return resolve(`../node_modules/pokemon-sprites/sprites/pokemon/${index}.png`)
+            resolvePath = `../node_modules/pokemon-sprites/sprites/pokemon/${index}.png`
         case ImageSource.item_sprite:
             break;
     }
 
+    if (resolvePath != "" && existsSync(resolvePath)) {
+        return resolve(resolvePath)
+    } else { 
+        return resolve(nothing)
+    }
 }
